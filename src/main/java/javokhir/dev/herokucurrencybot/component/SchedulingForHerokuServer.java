3fbf5e7f2aee952023b2885c08a6687c101971c5
Javohir <javohirfoziljonov2002@gmail.com>
2022-07-29 15:03:28 +0500

    commit

--- src/main/java/javokhir/dev/herokucurrencybot/component/SchedulingForHerokuServer.java
+++ src/main/java/javokhir/dev/herokucurrencybot/component/SchedulingForHerokuServer.java
@@ -1,19 +1,24 @@
 package javokhir.dev.herokucurrencybot.component;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 @Slf4j
 public class SchedulingForHerokuServer {
 
-//    @Scheduled(fixedRateString = "1200000") //20 minut
-//    public void PingMe(){
-//        try {
-//            URL url = new URL("https://currency-telegram-bot-2.herokuapp.com/ping");
-//            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-//            connection.connect();
-//            log.info("Ping{}, OK.response code{}", url.getHost(), connection.getResponseCode());
-//        }   catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//    }
+    @Scheduled(fixedRateString = "1200000") //20 minut
+    public void PingMe(){
+        try {
+            URL url = new URL("https://currency-telegram-bot-2.herokuapp.com/ping");
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.connect();
+            log.info("Ping{}, OK.response code{}", url.getHost(), connection.getResponseCode());
+        }   catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
