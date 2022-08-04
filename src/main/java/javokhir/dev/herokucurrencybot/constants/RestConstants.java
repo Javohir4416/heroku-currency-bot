ab6f4940d3b30d7bb85710cce4d1f4608cf200ab
Javohir <javohirfoziljonov2002@gmail.com>
2022-08-04 15:41:59 +0500

    commit

--- src/main/java/javokhir/dev/herokucurrencybot/constants/RestConstants.java
+++ src/main/java/javokhir/dev/herokucurrencybot/constants/RestConstants.java
@@ -3,6 +3,6 @@ package javokhir.dev.herokucurrencybot.constants;
 public interface RestConstants {
     String TELEGRAM_BASE_URL_WITH_BOT="https://api.telegram.org/bot";
     String TELEGRAM_BASE_URL="https://api.telegram.org";
-    String BOT_TOKEN="5076755668:AAGSQDPSWk_TVuwIXxveVq2j1tILw55UkJQ";
-    String PASSWORD="currency4416";
+    String BOT_TOKEN="5076755668:AAGBZNCPCVDowAY4L24JHJJJ8Br2_DJ1LDM";
+    String PASSWORD="menman";
 }
