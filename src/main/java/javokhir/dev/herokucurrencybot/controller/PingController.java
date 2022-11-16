434c2d90ad5a8483375e3a5c530d246257ffe70c
Javohir <javohirfoziljonov2002@gmail.com>
2022-11-16 16:04:05 +0500

    commit

--- src/main/java/javokhir/dev/herokucurrencybot/controller/PingController.java
+++ src/main/java/javokhir/dev/herokucurrencybot/controller/PingController.java
@@ -14,11 +14,20 @@ public class PingController {
     public Category ping(){
         Category category=new Category();
         CategoryItem categoryItem1=new CategoryItem();
-        categoryItem1.setCategoryId("54564");
-        categoryItem1.setCategoryName("ald");
+        categoryItem1.setCategoryId("1");
+        categoryItem1.setCategoryName("Birinchi");
         CategoryItem categoryItem2=new CategoryItem();
-        categoryItem2.setCategoryId("15621");
-        categoryItem2.setCategoryName("hwdbjnakm");
+        categoryItem2.setCategoryId("2");
+        categoryItem2.setCategoryName("Ikkinchi");
+        CategoryItem categoryItem3=new CategoryItem();
+        categoryItem3.setCategoryId("3");
+        categoryItem3.setCategoryName("Uchinchi");
+        CategoryItem categoryItem4=new CategoryItem();
+        categoryItem4.setCategoryId("4");
+        categoryItem4.setCategoryName("To'rtinchi");
+        CategoryItem categoryItem5=new CategoryItem();
+        categoryItem5.setCategoryId("5");
+        categoryItem5.setCategoryName("Beshinchi");
         ArrayList<CategoryItem> categoryItemArrayList=new ArrayList<>();
         categoryItemArrayList.add(categoryItem1);
         categoryItemArrayList.add(categoryItem2);
